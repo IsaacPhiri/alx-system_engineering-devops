dd6c4b7cdba0ee44dfb5d569cfb7a908bee377e0
Isaac Phiri <isaacphiri315@gmail.com>
2022-01-03 18:11:47 -0800

    Edited kill process file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,6 @@
-# Executes a command
-exec { 'pkill killmenow':
-  path => '/usr/bin:/usr/sbin:/bin'
+# Kills a process named killmenow
+exec { 'killmenow':
+  command => 'pkill -n killmenow',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test `pgrep killmenow|wc -l` -ge 1',
 }
