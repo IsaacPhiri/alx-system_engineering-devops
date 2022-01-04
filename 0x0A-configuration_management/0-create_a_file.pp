ec1f40f0d5b744c17cc027ec489c7d36733d1108
Isaac Phiri <isaacphiri315@gmail.com>
2022-01-03 18:07:04 -0800

    Edited code in create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creates a file
-file { '/tmp/school':
+# creates a file
+file {'school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
   mode    => '0744',
-
 }
