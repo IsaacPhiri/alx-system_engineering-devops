032be57d50b2bb349b1a5529bdf059247b3ae755
Isaac Phiri <isaacphiri315@gmail.com>
2022-12-14 04:42:59 -0800

    Edited errors on path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 #create a file in /tmp
-file { 'school':
-	path	=> '/tmp/school',
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I Love Puppet',
+file { '/tmp/school':
+  mode  => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+content => 'I Love Puppet',
 }
